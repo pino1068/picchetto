a86b1075420b1b3aedc2d612902a23f2a9caa9ee
Giuseppe Di Pierri <giuseppe.dipierri@gmtech.ch>
2016-08-05 18:41:14 +0200

    fixed of dario's email error, fixed that report wasn't working (because of security)

--- webapp/picchetto/grails-app/conf/SecurityFilters.groovy
+++ webapp/picchetto/grails-app/conf/SecurityFilters.groovy
@@ -1,12 +1,12 @@
 
 class SecurityFilters {
 	def filters = {
-		allowAssets(uri: '/*') {
+		rootstuff(uri: '/*') {
 			before = {
-				if (!session.user && request.requestURI == "/picchetto/") {
-					redirect(controller: 'login', action:"index")
-					return true
-				}
+					if (!session.user && request.requestURI == "/picchetto/") {
+						redirect(controller: 'login', action:"index")
+						return true
+					}
 			}
 		}
 		loginCheck(controller: '*', action: 'index', controllerExclude:"login") {
@@ -17,15 +17,15 @@ class SecurityFilters {
 				}
 			}
 		}
-		userCheck(controller: '*', action: 'search|login|logout|json', invert:true) {
+		userCheck(controller: '*', action: 'search|login|logout|json|people', invert:true) {
 			before = {
-					if(controllerName=='login'){
+					if(controllerName in ['login', "report"]){
 						return true
 					}
 					else if(controllerName=='period' && actionName in ["list", "buy", "sell"]){
 						return true
 					}
-					else if (!session.user?.admin  && !request.requestURI.contains("assets")) {
+					else if (!session.user?.admin  && !request.requestURI.contains("assets") && request.requestURI != "/picchetto/") {
 						redirect(controller: 'period', action:"search")
 						return true
 					}
